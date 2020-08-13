ea572395c88a86e7d6fb1b1a9f5927e86f94e49f
ali thabet <alithabet317@gmail.com>
2020-08-13 19:59:19 -0400

    third commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// let divRef = document.getElementById("human");
+// let divRef2 = document.getElementById("relief");
+// let divRef3 = document.getElementById("child");
+// let divRef4 = document.getElementById("civil");
+// divRef.onmouseover= function() {
+//     divRef.style.width = "488px";
+//     divRef.style.height = "684px";
+// }
+// divRef.onmouseout = function() {
+//     divRef.style.width = "374px";
+//     divRef.style.height = "604px";
+// }
+// divRef2.onmouseover= function() {
+//     divRef2.style.width = "488px";
+//     divRef2.style.height = "684px";
+// }
+// divRef2.onmouseout = function() {
+//     divRef2.style.width = "374px";
+//     divRef2.style.height = "604px";
+// }
+// divRef3.onmouseover= function() {
+//     divRef3.style.width = "488px";
+//     divRef3.style.height = "684px";
+// }
+// divRef3.onmouseout = function() {
+//     divRef3.style.width = "374px";
+//     divRef3.style.height = "604px";
+// }
+// divRef4.onmouseover= function() {
+//     divRef4.style.width = "488px";
+//     divRef4.style.height = "684px";
+// }
+// divRef4.onmouseout = function() {
+//     divRef4.style.width = "374px";
+//     divRef4.style.height = "604px";
+// }
